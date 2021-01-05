2d47240b232c5b5f372c170181393984eba4b416
scedastic <yosef.sheinfil@gmail.com>
2021-01-05 11:34:29 -0500

    create base classes

---
+++ tvm/tvm_annuity.py
@@ -0,0 +1,4 @@
+class TVMAnnuity(TVMBase):
+    def __init__(self):
+        super().__init__()
+        self.cash_flow = 0.0
---
+++ tvm/tvm_base.py
@@ -0,0 +1,9 @@
+class TVMBase:
+    def __init__(self):
+        self.start_value = 0.0
+        self.rate = 0.0
+        self.periods = 0
+        self.end_value = 0.0
+
+    def calculate(self):
+        pass
